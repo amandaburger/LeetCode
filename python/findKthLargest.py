5e76bcb798087299251908e7e19f6f37ca07a8f6
Amanda Burger <amandaburger01@gmail.com>
2025-02-17 13:43:47 -0500

    kth largest

--- python/findKthLargest.py
+++ python/findKthLargest.py
@@ -1,14 +1,14 @@
 import heapq
 
-def findKthLargest(k,nums):
+def findKthLargest(nums,k):
     heap = nums[:k]
+    print(heap)
     heapq.heapify(heap)
     
     for num in nums[k:]:
         if num > heap[0]:
             heapq.heappop(heap)
             heapq.heappush(heap, num)
-        print(heap)
     return heap[0]
 print(findKthLargest([3,2,1,5,6,4],2))
 print(findKthLargest([3,2,3,1,2,4,5,5,6],4))
